ba92124aaadab90a3b03b5f1c020dcaa4ea82b39
sumukhvaidya <sumukhvaidya@gmail.com>
2025-08-29 15:56:36 -0400

    Implemented Rabi Oscillatinon code in ODMR

--- dev/ODMR.py
+++ dev/ODMR.py
@@ -60,54 +60,89 @@ class ODMR:
         if self.sg386 is None:
             raise RuntimeError("SG386 not set. Call set_instruments() or pass sg386 to __init__")
     
-    def create_pulse_sequence(self, aomvolt=None):
+    def create_pulse_sequence(self, aomvolt=None, pulse_sequence=None):
         """Create pulse sequence for ODMR measurement"""
         self.validate_instruments()
         
         aomvolt = aomvolt or self.config['aomvolt']
+        pulse_sequence = pulse_sequence or self.config['pulse_sequence']
         
-        # Create pulse sequence
-        ch0patt = [(self.config['wait_t'], 1), 
-                   (self.config['count_t'], 1), 
-                   (self.config['addl_t'], 1),
-                   (self.config['wait_t'], 1), 
-                   (self.config['count_t'], 1), 
-                   (self.config['addl_t'], 1),
-                   (self.config['separation_t'], 1)]  # Laser always ON
-        ch1patt = [(self.config['wait_t'], 1), 
-                   (self.config['count_t'], 1), 
-                   (self.config['addl_t'], 1),
-                   (self.config['wait_t'], 0), 
-                   (self.config['count_t'], 0), 
-                   (self.config['addl_t'], 0),
-                   (self.config['separation_t'], 0)]
-        ch2patt = [(self.config['wait_t'], 0), 
-                   (self.config['count_t'], 1), 
-                   (self.config['addl_t'], 0),
-                   (self.config['wait_t'], 0), 
-                   (self.config['count_t'], 1), 
-                   (self.config['addl_t'], 0),
-                   (self.config['separation_t'], 0)]
+        # Create pulse sequence. Default is CW ODMR
+        if pulse_sequence is None or pulse_sequence == 'cw_odmr':
+            # Ch0: Laser
+            ch0patt = [(self.config['wait_t'], 1), 
+                    (self.config['count_t'], 1), 
+                    (self.config['addl_t'], 1),
+                    (self.config['wait_t'], 1), 
+                    (self.config['count_t'], 1), 
+                    (self.config['addl_t'], 1),
+                    (self.config['separation_t'], 1)]  # Laser always ON
+            # CH1: MW 
+            ch1patt = [(self.config['wait_t'], 0), 
+                    (self.config['count_t'], 0), 
+                    (self.config['addl_t'], 0),
+                    (self.config['wait_t'], 1), 
+                    (self.config['count_t'], 1), 
+                    (self.config['addl_t'], 1),
+                    (self.config['separation_t'], 0)]
+            # CH2: Counter
+            ch2patt = [(self.config['wait_t'], 0), 
+                    (self.config['count_t'], 1), 
+                    (self.config['addl_t'], 0),
+                    (self.config['wait_t'], 0), 
+                    (self.config['count_t'], 1), 
+                    (self.config['addl_t'], 0),
+                    (self.config['separation_t'], 0)]
+            
+            seq = self.ps.createSequence()
+            seq.setDigital(0, ch0patt)
+            seq.setDigital(1, ch1patt)
+            seq.setDigital(2, ch2patt)
         
-        seq = self.ps.createSequence()
-        seq.setDigital(0, ch0patt)
-        seq.setDigital(1, ch1patt)
-        seq.setDigital(2, ch2patt)
-        
+        if pulse_sequence == 'rabi':
+            # Ch0: Laser
+            ch0patt = [(self.config['init_t'], 1), 
+                    (self.config['wait_t'], 0), 
+                    (self.config['delay_t'], 1),
+                    (self.config['readout_t'], 1), 
+                    (self.config['init_t'], 1), 
+                    (self.config['wait_t'], 0), 
+                    (self.config['delay_t'], 1),
+                    (self.config['readout_t'], 1),]
+            # CH1: MW
+            ch1patt = [(self.config['init_t']+self.config['wait_t']+self.config['delay_t']+self.config['readout_t'], 0),
+                    (self.config['init_t']+self.config['wait_t']+self.config['delay_t']-70-self.config['tau_set'], 0), 
+                    (self.config['tau_set'], 1),
+                    (self.config['readout_t']+70, 0),]
+            # CH2: Counter
+            ch2patt =  [(self.config['init_t'], 0), 
+                    (self.config['wait_t'], 0), 
+                    (self.config['delay_t'], 0),
+                    (self.config['readout_t'], 1), 
+                    (self.config['init_t'], 0), 
+                    (self.config['wait_t'], 0), 
+                    (self.config['delay_t'], 0),
+                    (self.config['readout_t'], 1),] 
+            seq = self.ps.createSequence()
+            seq.setDigital(0, ch0patt)
+            seq.setDigital(1, ch1patt)
+            seq.setDigital(2, ch2patt)
+
         return seq
     
-    def get_contrast(self, freq=None, plot_sequence=False):
+    def get_contrast(self, freq=None, plot_sequence=False, config = None):
         """Get ODMR contrast at specified frequency"""
         self.validate_instruments()
-        
-        freq = freq or self.config['freq']
-        
+        config = config or self.config
+
+        freq = freq or config['freq']
+
         # Update frequency
         self.sg386.write(f'FREQ {freq}')
-        self.sg386.write('AMPR '+str(self.config['mw_power']) )
-        
+        self.sg386.write('AMPR '+str(config['mw_power']) )
+
         # Create pulse sequence
-        seq = self.create_pulse_sequence()
+        seq = self.create_pulse_sequence(pulse_sequence=self.config['pulse_sequence'])
         if plot_sequence:
             # seq.plot()
             pass
@@ -121,8 +156,8 @@ class ODMR:
 
             # Calculate x, y voltage for fixing position
             xy_volts = np.array([
-            (self.config['position'][0] - self.config['samples_per_axis'] // 2) * self.config['scale'],
-            (self.config['position'][1] - self.config['samples_per_axis'] // 2) * self.config['scale']])
+            (config['position'][0] - config['samples_per_axis'] // 2) * config['scale'],
+            (config['position'][1] - config['samples_per_axis'] // 2) * config['scale']])
              # Write the position to the FPGA
             # host2target = session.fifos['FIFO_Host2Target']
             # host2target.write(xy_volts)
@@ -131,16 +166,18 @@ class ODMR:
 
             # Setup readout of the counts
             target2host = session.fifos['FIFO_target2host']
-            target2host.configure(self.config['pulsenum'])
+            target2host.configure(config['pulsenum'])
             target2host.start()
-            
-            
-            self.ps.stream(seq, n_runs=int(self.config['pulsenum']/2)) # Run the experiment
-            read_value = target2host.read(self.config['pulsenum'], 1000) # Read the counts as an array
+
+
+            self.ps.stream(seq, n_runs=(config['pulsenum'])//2) # Run the experiment
+            read_value = target2host.read(config['pulsenum'], 1000) # Read the counts as an array
+            # read_value = target2host.read(config['pulsenum']*2, 1000) # Read the counts as an array
             target2host.stop() # Stop the register
             
             counts = read_value[0]
-            contrast = sum(counts[0::2]) / sum(counts[1::2])
+            # contrast = (sum(counts[0::2]) * (self.config['pulsenum']//2) / (sum(counts[1::2])*(self.config['pulsenum']//2+1)))
+            contrast = sum(counts[3::2]) / sum(counts[2::2])
             
         return contrast, sum(counts[0::2])*1e9/(len(counts)*self.config['count_t']), sum(counts[1::2])*1e9/(len(counts)*self.config['count_t'])
     
@@ -193,13 +230,64 @@ class ODMR:
             all_counts[j,:] = mw_on+mw_off
         
         return freq_range, avg_contrast, all_contrast, avg_counts, all_counts
+    
+    def run_rabi_sweep(self, tau_range, plot_sequence_first=True):
+        """Run ODMR measurement over a frequency range"""
+        self.validate_instruments()
+
+
+        all_contrast = np.zeros((self.config['num_avgs'],len(tau_range)))
+        avg_contrast = np.zeros(len(tau_range))
+        all_counts = np.zeros((self.config['num_avgs'],len(tau_range)))
+        avg_counts = np.zeros(len(tau_range))
+
+
+        for j in tqdm(range(self.config['num_avgs'])):
+            contrast = np.zeros(len(tau_range))
+            mw_on = np.zeros(len(tau_range))
+            mw_off = np.zeros(len(tau_range))
+
+            # for i, freq in enumerate(tqdm(freq_range, desc="Frequency sweep")):
+            for i, tau in enumerate(tau_range):
+                self.config['tau_set'] = tau
+                plot_seq = plot_sequence_first and i == 0
+                
+                if keyboard.is_pressed('q'):
+                    print(f"\nMeasurement stopped by user at tau = {tau:.2e} ns")
+                    # Return partial results up to current point
+                    return tau_range, avg_contrast, all_contrast, avg_counts, all_counts
+
+                try:
+                    contrast[i], mw_on[i], mw_off[i] = self.get_contrast( plot_sequence=plot_seq)
+                    
+                    fig, ax = plt.subplots(2, 2, figsize=(16,12))
+                    clear_output(wait=True)
+                    fig.suptitle(f'Rabi Measurement - Run {j+1}/{self.config["num_avgs"]}. Press Q to stop', fontsize=22, y=0.95)
+
+                    plot_this_xy(tau_range[:i], contrast[:i], ax = ax[0][0], title = 'Current Run Rabi Contrast', xlabel = 'Tau (ns)', ylabel = 'Contrast', linestyle = '-', color = 'blue', linewidth = 2, marker = 'o', markersize = 2)
+                    plot_this_xy(tau_range, avg_contrast, ax = ax[0][1], title = 'Average Rabi Contrast', xlabel = 'Tau (ns)', ylabel = 'Average Contrast', linestyle = '-', color = 'blue', linewidth = 2, marker = 'o', markersize = 2)
+
+                    plot_this_xy(tau_range[:i], mw_on[:i]+mw_off[:i], ax = ax[1][0], title = 'Average Rabi Counts', xlabel = 'Tau (ns)', ylabel = 'Average Counts', linestyle = '-', color = 'blue', linewidth = 2, marker = 'o', markersize = 2)
+                    plot_this_xy(tau_range, avg_counts, ax = ax[1][1], title = 'Average Rabi Counts', xlabel = 'Tau (ns)', ylabel = 'Average Counts', linestyle = '-', color = 'blue', linewidth = 2, marker = 'o', markersize = 2)
+
+                    plt.show()
+                    plt.pause(0.01)
+                except Exception as e:
+                    print(f"Error at tau {tau}: {e}")
+                    continue
+            avg_contrast = (avg_contrast*(j)+contrast)/(j+1)
+            all_contrast[j,:] = contrast
+            avg_counts = (avg_counts*(j)+mw_on+mw_off)/(j+1)
+            all_counts[j,:] = mw_on+mw_off
+
+        return tau_range, avg_contrast, all_contrast, avg_counts, all_counts
 
     def peakfinder_ODMR(self, config, window = 1, plotting=True):
         if not config:
             config = self.config
         
         center = config['position']
-        contrasts = np.zeros([window, window])
+        contrasts = np.ones([window, window])
         for i in range(window):
             for j in range(window):
                 config['position'] = [center[0]+i-window//2, center[1]+j-window//2]
@@ -213,8 +301,8 @@ class ODMR:
                     fig.suptitle(f'Running...', fontsize=22, y=0.95)
                     plt.pause(0.01)
                     # plt.imshow(contrasts, cmap=LiLabColormap, extent=(center[0]-window//2, center[0]+window//2, center[1]-window//2, center[1]+window//2))
-
-        imax, jmax = np.unravel_index(np.argmax(abs(contrasts)), contrasts.shape)
+        ############!!!!!!!!!!!!!!!! get_contrast needs to pass its own config!
+        imax, jmax = np.unravel_index(np.argmax(abs(contrasts-np.ones((window, window)))), contrasts.shape)
         config['position'] = [center[0]+imax-window//2, center[1]+jmax-window//2]
         print('Best Contrast = ', self.get_contrast()[0])
 
